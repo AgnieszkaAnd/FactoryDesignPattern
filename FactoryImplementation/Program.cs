3090c3193af3a50070163af27a042f6b2dc230c4
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-06-27 00:04:45 +0200

    second implementation

--- FactoryImplementation/Program.cs
+++ FactoryImplementation/Program.cs
@@ -6,6 +6,7 @@ using System;
 namespace FactoryImplementation {
     class Program {
         static void Main(string[] args) {
+
             Console.WriteLine("Hello Factory!");
 
             DocumentCreator cv = new CVCreator();
